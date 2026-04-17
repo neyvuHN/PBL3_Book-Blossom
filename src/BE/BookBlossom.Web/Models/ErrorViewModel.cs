c19e49045354b0b6d52e4db1276bbce4a926155a
Hoai Nhu <nhutranthihoai35@gmail.com>
2026-04-17 17:05:13 +0700

    Tạo project MVC cho BE

---
+++ src/BE/BookBlossom.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookBlossom.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
