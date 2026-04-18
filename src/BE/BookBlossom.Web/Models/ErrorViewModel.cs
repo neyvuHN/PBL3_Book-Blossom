863d7de094978dd664a077e635a32310ac098d82
TyTrinh <trinhtran180206@gmail.com>
2026-04-18 08:47:53 +0700

    Lưu tạm (ko quan trọng)

--- src/BE/BookBlossom.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BookBlossom.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
